bd0d32e9ccb85be976bee34b8a1a5b6840a0d8c4
Tristan Gordon <tgordon@athlonsports.com>
2016-09-08 13:23:41 -0500

    added facebook api, and functions to normalize objects

--- server.js
+++ server.js
@@ -41,8 +41,9 @@ apiRoutes.post('/setHash', function(req, res){
 });
  
 // instagram object request
-apiRoutes.get('/getSocial', function(req, res) {
+apiRoutes.get('/getInsta', function(req, res) {
 
+//grab hashtag value saved in browser cookie, if this doesn't exist, use 'athlon'
 var hashtag     = (req.cookies['hashtag']) ? (req.cookies['hashtag']).replace(/^.*#/, '') : 'athlon';
 
 var accessToken = '3678064701.99fdca0.ef182a248bc343b38e27912405be7945',
@@ -50,7 +51,20 @@ var accessToken = '3678064701.99fdca0.ef182a248bc343b38e27912405be7945',
     endpoint    = 'https://api.instagram.com/v1/tags/'+ tag + '/media/recent?access_token=' + accessToken + '&access_token='+ accessToken;
     request(endpoint, function(err, response, body){
         res.send(body);
+        console.log(body);
     });
+});
+
+apiRoutes.get('/getFacebook', function(req, res) {
+
+var accessToken = 'EAASbZB8ZAYlu0BAHO2t9U0X7HKmkOGA5GMBE0E9iZCk2Mc2dQoRSeB3tnCkj6tjLiXAZCFdvYOJYkPQCRQBqXQuZA07ipCoZAkZAzawTr2VJ3xjaT2eEL9FL8zgdfa7ZCDUyPDkuufyoZAI94R0Lb0ZCWEBt7YvdFGwu9VjgXbCxFzqgZDZD',
+    pageId      = '293589274343115',
+    endpoint    = 'https://graph.facebook.com/v2.7/' + pageId + '/photos?fields=images,link,from,created_time&type=uploaded&access_token=' + accessToken; 
+
+    request(endpoint, function(err, response, body) {
+        res.send(body);
+        console.log(body);
+    });  
 });
  
 // will eventually connect the api routes under /sima-api
--- socialApp.js
+++ socialApp.js
@@ -1,15 +1,114 @@
+function convertDate (date) {
+	var thisDate = new Date(date);
+	var properFormat = thisDate.getTime();
+
+	return properFormat/1000;
+}
+
+function sortByCreatedTime (socialArray) {
+	socialArray.sort(function(a, b) {
+    	return parseFloat(b.created) - parseFloat(a.created);
+	});
+
+	console.log(socialArray);
+	return socialArray;
+}
+
 angular.module('socialApp', [])
 	.controller('SocialPhotoController', function ($http){
-	var social 			  = this;
-		social.endpoint    = '/getSocial';
+		var social 	  = this,
+		socialCards   = [],
+		endpoint      = '/getInsta',
+		endpoint2     = '/getFacebook';
+		count	  	  = 0;
 
 		$http({
 			method: 'GET',
-			url: social.endpoint
+			url: endpoint
 		})
 			.then(function(response) {
-				social.data = response.data.data;
+				var data = response.data.data;
+
+				//take data from object and normalize so that it works with all
+				//sources
+				for (i = 0; i < data.length; i++) {
+					var socialCard = {
+						postLink: data[i].link,
+						sourceUrl: data[i].images.standard_resolution.url,
+						socialLogo: 'instagramlogo.png',
+						profilePicSource: data[i].user.profile_picture,
+						profileLink:'https://www.instagram.com/' + data[i].user.username,
+						username: data[i].user.username,
+						source: 'instagram',
+						created: parseInt(data[i].caption.created_time)
+					}
+
+					socialCards.push(socialCard);
+
+				}
+				//use count variable to signal that request is done
+				count += 1;
+				return count;
+
 			}, function (response){
-				error('broken!');
+				error('broken!' + response);
+			}).then(function (){
+				// check to see if all requests are finished before sorting
+				//and sending data to view
+				if (count == 1) {
+					
+				}
+				else {
+					social.data = sortByCreatedTime(socialCards);
+				}
 			});
+
+		$http({
+			method: 'GET',
+			url: endpoint2
+		})
+			.then(function(response) {
+				dataTwo = response.data.data;
+
+				console.log(dataTwo);
+
+				//take data from object and create uniform object so that our markup
+				//works with all photos, not just instagram
+				for (i = 0; i < dataTwo.length; i++) {
+					var socialCard = {
+						postLink: dataTwo[i].link,
+						sourceUrl: dataTwo[i].images[0].source,
+						socialLogo: 'facebooklogo.png',
+						profilePicSource: dataTwo[i].images[0].source,
+						profileLink:'https://www.facebook.com/' + dataTwo[i].from.id,
+						username: dataTwo[i].from.name,
+						source: 'facebook',
+						created: convertDate(dataTwo[i].created_time)
+					}
+
+					socialCards.push(socialCard);
+
+				}
+				//use count variable to signal that request is done
+				count += 1;
+				return count;
+
+			}, function (response){
+				error('broken!' + response);
+			}).then(function (){
+				// check to see if all requests are finished before sorting
+				//and sending data to view
+				if (count == 1) {
+					
+				}
+				else {
+					social.data = sortByCreatedTime(socialCards);
+				}
+			});
+
+
+	
+
+		
+
 	});
